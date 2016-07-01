c5637495aec7f89be1be3ed926c6b3c768aedc59
Tim Field <tim@thefold.co.nz>
2016-07-01 16:14:33 +1200

    include warning for old php versions

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: ThisData for WordPress
 Plugin URI: https://thisdata.com/
-Version: 1.0.1c
+Version: 1.1.0
 Description: ThisData provides login intelligence. We notify you when one of your users or customers has their account accessed from somewhere unusual or by a device they don't normally use. It gives your customers the confidence that you're taking security seriously and doing everything you can to protect their account.
 Text Domain: thisdata-plugin
 License: GPL
@@ -20,39 +20,54 @@ const ENV_JS_WRITE_KEY = 'THISDATA_JS_WRITE_KEY';
 const ENV_JS_SIGNATURE = 'THISDATA_JS_SIGNATURE';
 const ENV_WEBHOOK_SIGNATURE = 'THISDATA_WEBHOOK_SIGNATURE';
 
-if( is_admin() ) {
-    $admin = new Admin();
-    $admin->init();
-    $admin->render();
-}
 
-add_action('init', function() {
+if(version_compare(\PHP_VERSION, '5.5', '<' )) {
 
-    if($apiKey = API::getKey()) {
+    add_action('admin_notices', function() {
+        $message = sprintf(
+        sprintf(__('Sorry your PHP version is too old. The ThisData plugin requires PHP >= 5.5. You are running version %s.','thisdata-plugin'), \PHP_VERSION),
+        Admin::getSettingsPageURL());
 
-        try {
+        echo '<div class="notice notice-info"><p>'.$message.'</p></div>';
+    });
 
-            Events::init(API::getEventsEndpoint());
-            Webhook::init();
-            JS::init();
+} else {
 
-        } catch (\Exception $e) {
+    if( is_admin() ) {
+        $admin = new Admin();
+        $admin->init();
+        $admin->render();
+    }
 
-            add_action('admin_notices', function() use ($e) {
-                $message = $e->getMessage();
-                echo '<div class="notice notice-error"><p>'.$message.'</p></div>';
+    add_action('init', function() {
+
+        if($apiKey = API::getKey()) {
+
+            try {
+
+                Events::init(API::getEventsEndpoint());
+                Webhook::init();
+                JS::init();
+
+            } catch (\Exception $e) {
+
+                add_action('admin_notices', function() use ($e) {
+                    $message = $e->getMessage();
+                    echo '<div class="notice notice-error"><p>'.$message.'</p></div>';
+                });
+            }
+
+        } elseif(empty($_GET['page']) || $_GET['page'] !== Admin::SETTINGS) {
+
+            add_action('admin_notices', function() {
+                $message = sprintf(
+                __('Almost done, please enter your <a href="%s">ThisData API Key</a> to complete the installation.','thisdata-plugin'),
+                Admin::getSettingsPageURL());
+
+                echo '<div class="notice notice-info"><p>'.$message.'</p></div>';
             });
         }
 
-    } elseif(empty($_GET['page']) || $_GET['page'] !== Admin::SETTINGS) {
+    });
 
-        add_action('admin_notices', function() {
-            $message = sprintf(
-            __('Almost done, please enter your <a href="%s">ThisData API Key</a> to complete the installation.','thisdata-plugin'),
-            Admin::getSettingsPageURL());
-
-            echo '<div class="notice notice-info"><p>'.$message.'</p></div>';
-        });
-    }
-
-});
+}
